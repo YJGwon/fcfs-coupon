d2ea218a43f38b0fbe2e8cd0c6b4d8bf0a714a2c
Forky <trulyforky@gmail.com>
2023-05-11 23:54:48 +0900

    [Infra] embedded Redis 설정 (#6)
    * chore: embedded redis, spring data redis 의존성 추가
    
    * feat: application 실행 시 embedded redis 서버 시작
    
    * fix: 이미 embedded redis 실행중일 때 포트 충돌 해결
    
    해당 포트 확인하는 cmd 실행 후 사용중이면 서버 시작하지 않음

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,15 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	// spring data redis
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	// embedded-redis
+	implementation("it.ozimov:embedded-redis:0.7.2")
+
+	// rest-assured
 	testImplementation("io.rest-assured:rest-assured:5.3.0")
 }
 
