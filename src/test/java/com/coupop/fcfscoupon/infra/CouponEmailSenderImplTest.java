9d61c7a2db42cf671f0da144586c12f8d4d0260a
Forky <trulyforky@gmail.com>
2023-05-18 22:58:33 +0900

    feat: 쿠폰 값 고유 코드로 변경 (#26)
    * feat: 쿠폰 값 고유한 코드로 생성
    
    * refactor: secret key field RandomCodeGeneratorImpl로 이동

--- src/test/java/com/coupop/fcfscoupon/infra/CouponEmailSenderImplTest.java
+++ src/test/java/com/coupop/fcfscoupon/infra/CouponEmailSenderImplTest.java
@@ -18,7 +18,7 @@ class CouponEmailSenderImplTest {
     @DisplayName("쿠폰을 메일로 발송한다.")
     @Test
     void send() {
-        final Coupon coupon = new Coupon("쿠폰");
+        final Coupon coupon = new Coupon("뭔가 좋은 쿠폰");
         final String toAddress = "yj970125@gmail.com";
 
         couponEmailSender.send(coupon, toAddress);
