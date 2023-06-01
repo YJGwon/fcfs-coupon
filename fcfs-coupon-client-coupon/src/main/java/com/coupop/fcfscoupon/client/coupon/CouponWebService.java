fb00042920e014a501ad77520af424587d3416b0
Forky <trulyforky@gmail.com>
2023-06-01 13:06:22 +0900

    feat(client-coupon): 요청 header 추가, 요청 및 에러 응답 처리 테스트 추가 (#51)
    * test(client-coupon): mockserver로 client가 전송하는 요청 정확하게 검증
    
    * feat(client-coupon): client 요청 contentType, accept header 추가
    
    * test(client-coupon): 서버 에러 응답 시 예외 발생 case 추가

--- fcfs-coupon-client-coupon/src/main/java/com/coupop/fcfscoupon/client/coupon/CouponWebService.java
+++ fcfs-coupon-client-coupon/src/main/java/com/coupop/fcfscoupon/client/coupon/CouponWebService.java
@@ -2,9 +2,12 @@ package com.coupop.fcfscoupon.client.coupon;
 
 import com.coupop.fcfscoupon.api.coupon.dto.IssuanceRequest;
 import com.coupop.fcfscoupon.api.coupon.dto.ResendRequest;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
+@HttpExchange(contentType = MediaType.APPLICATION_JSON_VALUE, accept = MediaType.APPLICATION_JSON_VALUE)
 interface CouponWebService {
 
     @PostExchange("/issue")
--- fcfs-coupon-client-coupon/src/test/java/com/coupop/fcfscoupon/client/coupon/CouponServiceTest.java
+++ fcfs-coupon-client-coupon/src/test/java/com/coupop/fcfscoupon/client/coupon/CouponServiceTest.java
@@ -1,74 +1,110 @@
 package com.coupop.fcfscoupon.client.coupon;
 
-import java.io.IOException;
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
-import org.jetbrains.annotations.NotNull;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockserver.model.HttpRequest.request;
+
+import com.coupop.fcfscoupon.api.coupon.dto.IssuanceRequest;
+import com.coupop.fcfscoupon.api.coupon.dto.ResendRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.MediaType;
+import org.mockserver.verify.VerificationTimes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @SpringBootTest
 class CouponServiceTest {
 
+    @Autowired
     private CouponService couponService;
-    private MockWebServer mockWebServer;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private ClientAndServer mockServer;
 
     @BeforeEach
-    void startMockServer() {
-        mockWebServer = startServer();
-
-        final WebClient client = WebClient.builder()
-                .baseUrl(mockWebServer.url("/").toString())
-                .build();
-        final HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client))
-                .build();
-        couponService = new CouponService(factory.createClient(CouponWebService.class));
+    void setUp() {
+        mockServer = ClientAndServer.startClientAndServer(8081);
     }
 
     @AfterEach
-    void closeMockServer() throws IOException {
-        mockWebServer.close();
+    void stop() {
+        mockServer.stop();
     }
 
     @DisplayName("설정된 url로 쿠폰 발급 요청을 전송한다.")
     @Test
-    void issue() {
-        couponService.issue(1L, "foo@bar.com");
+    void issue() throws JsonProcessingException {
+        // given
+        final long seq = 1L;
+        final String email = "foo@bar.com";
+
+        // when
+        couponService.issue(seq, email);
+
+        // then
+        mockServer.verify(
+                request()
+                        .withMethod("POST")
+                        .withPath("/issue")
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withHeader("accept", MediaType.APPLICATION_JSON.toString())
+                        .withBody(objectMapper.writeValueAsBytes(new IssuanceRequest(seq, email))),
+                VerificationTimes.once()
+        );
     }
 
     @DisplayName("설정된 url로 쿠폰 재발송 요청을 전송한다.")
     @Test
-    void resend() {
-        couponService.resend("fakeId");
+    void resend() throws JsonProcessingException {
+        // given
+        final String historyId = "fakeId";
+
+        // when
+        couponService.resend(historyId);
+
+        // then
+        mockServer.verify(
+                request()
+                        .withMethod("POST")
+                        .withPath("/resend")
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withHeader("accept", MediaType.APPLICATION_JSON.toString())
+                        .withBody(objectMapper.writeValueAsBytes(new ResendRequest(historyId))),
+                VerificationTimes.once()
+        );
     }
 
-    private MockWebServer startServer() {
-        final MockWebServer mockWebServer = new MockWebServer();
-        final MockResponse acceptedResponse = new MockResponse()
-                .setResponseCode(HttpStatus.ACCEPTED.value());
-        final Dispatcher dispatcher = new Dispatcher() {
-            @NotNull
-            @Override
-            public MockResponse dispatch(@NotNull final RecordedRequest recordedRequest) {
-                if (recordedRequest.getPath().contains("/issue")) {
-                    return acceptedResponse;
-                }
-                if (recordedRequest.getPath().contains("/resend")) {
-                    return acceptedResponse;
-                }
-                return new MockResponse().setResponseCode(HttpStatus.NOT_FOUND.value());
-            }
-        };
-        mockWebServer.setDispatcher(dispatcher);
-        return mockWebServer;
+    @DisplayName("서버에서 에러를 응답할 경우 예외가 발생한다.")
+    @Test
+    void throwsException_whenServerResponseError() {
+        // given
+        final String errorMessage = "서버에 오류가 발생했습니다.";
+        final String problemDetails = String.format("""
+                {
+                    "type" : "about:blank",
+                    "title": "Internal Server Error",
+                    "detail": "%s"
+                }""", errorMessage);
+        mockServer
+                .when(request())
+                .respond(
+                        HttpResponse.response()
+                                .withStatusCode(500)
+                                .withHeader("Content-Type", "application/problem+json; charset=utf-8")
+                                .withBody(problemDetails)
+                );
+
+        // when & then
+        assertThatExceptionOfType(CouponClientException.class)
+                .isThrownBy(() -> couponService.resend("fakeId"))
+                .withMessage(errorMessage);
     }
 }
