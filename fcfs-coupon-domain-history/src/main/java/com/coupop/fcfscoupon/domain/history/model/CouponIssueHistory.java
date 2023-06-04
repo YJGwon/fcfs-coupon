ed70805896a062bf9ddef71908fa4b987f1fa5bb
YJGwon <trulyforky@gmail.com>
2023-06-04 12:58:21 +0900

    chore: Entity class에 `@Data` 적용

--- fcfs-coupon-domain-history/src/main/java/com/coupop/fcfscoupon/domain/history/model/CouponIssueHistory.java
+++ fcfs-coupon-domain-history/src/main/java/com/coupop/fcfscoupon/domain/history/model/CouponIssueHistory.java
@@ -1,10 +1,12 @@
 package com.coupop.fcfscoupon.domain.history.model;
 
 import java.time.LocalDateTime;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Data
 public class CouponIssueHistory {
 
     private final String email;
@@ -14,29 +16,7 @@ public class CouponIssueHistory {
     @Id
     private String id;
 
-    private CouponIssueHistory(final String email, final String couponId, final LocalDateTime createdAt) {
-        this.email = email;
-        this.couponId = couponId;
-        this.createdAt = createdAt;
-    }
-
     public static CouponIssueHistory ofNew(final String email, final String couponId) {
         return new CouponIssueHistory(email, couponId, LocalDateTime.now());
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getCouponId() {
-        return couponId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
     }
 }
